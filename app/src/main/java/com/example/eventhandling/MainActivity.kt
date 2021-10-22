d9f4f7d7f8d0cf64dcf044d6a992d9af53385b0b
Tauhid <tauhid7870@gmail.com>
2021-10-22 21:39:44 +0530

    To handle the Event

---
+++ app/src/main/java/com/example/eventhandling/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.eventhandling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myName: TextView = findViewById(R.id.name)
+        val showName: Button = findViewById(R.id.show_name)
+
+        //region
+
+        val myMessage: EditText = findViewById(R.id.message)
+        val showMessage: Button = findViewById(R.id.show_message)
+
+        showMessage.setOnClickListener {
+            Toast.makeText(this, myMessage.text, Toast.LENGTH_LONG).show()
+        }
+
+        //endregion
+
+    }
+}
+
+
+        //val listener = object : View.OnClickListener {
+
+            //override fun onClick(v: View?) {
+               //myName.text = getString(R.string.my_name)
+
+        //showName.setOnClickListener(listener)
+
+
